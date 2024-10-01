0da5c21c3682a9197e956a8431a7f26e7048bc1e
Murali_Synergym <murali.rokalla@synergym.es>
2024-10-01 14:36:03 +0530

    Initial Commit

---
+++ SimpleElevator/SimpleElevatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleElevatorApp.swift
+//  SimpleElevator
+//
+//  Created by Murali on 01/10/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleElevatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
